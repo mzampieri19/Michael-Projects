33187215bf5547a511b4a92c7614b6f24239c56a
mzampieri19 <127122005+mzampieri19@users.noreply.github.com>
2024-08-08 14:16:35 -0400

    moved things

--- DTA/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Tabs from './components/Tabs.js';
-import { Calendar } from 'react-native-calendars';
-
-export default function App() {
-  return (
-      <Tabs> </Tabs>
-  );
-}
